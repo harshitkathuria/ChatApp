40ef275dff970ea7a809925b6c207b5285aac45b
harshitkathuria <harshitkathuria14@gmail.com>
2020-12-03 14:57:31 +0530

    Integrated electron js

---
+++ app.js
@@ -0,0 +1,38 @@
+// Modules
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    webPreferences: { nodeIntegration: true },
+    show: false
+  })
+
+  mainWindow.maximize();
+  mainWindow.show();
+
+  mainWindow.loadURL('http://localhost:3000')
+
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
